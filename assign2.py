aab223c0787883156fa7c547b6e8f9f42e5e8f6e
Mohammed Samee <86250719+samhk90@users.noreply.github.com>
2022-09-26 07:41:47 +0530

    Update assign2.py

--- assign2.py
+++ assign2.py
@@ -1,35 +1,38 @@
 print("***ASSINGMENT-02***")
 print("Enter Ab for absent student")
 n=int(input("Enter no. of students:: "))
-marks=[]
-m3=[]
-m2=[]
+marks={}
+m3={}
+m2={}
 sum=0
 count=0
 for i in range(0,n):
-  m=(input("Enter mraks:: "))
-  marks.append(m)  
+  z=input("enter name of student")
+  m=((input("Enter mraks:: ")))
+  marks.update({z:m}) 
   if m=="Ab":
-    m2.append(m)
+    m2.update({z:m})
   else:
-    z=int(m)
-    m3.append(z)
+    v=int(m)
+    m3.update({z:v}) 
 if m3==[]:
   print("whole class was absent")
 else:
- for j in range(0,len(m3)):
-   sum=sum+m3[j]
-for i in marks:
+ for j in m3.values():
+   sum=sum+j
+for i in marks.values():
    if i=="Ab":
      count=count+1
-z=n-count
-avg=sum/z
+k=n-count
+avg=sum/k
 print("1.Avg score of class:: ",avg)
-print("2.Highest marks in class:: ",max(m3))
-print("2.Lowest marks in class:: ",min(m3))
+print("2.Highest marks in class:: ",max(m3.values()))
+print("2.Lowest marks in class:: ",min(m3.values()))
 print("3.Number of students who was absent:: ",count)
-for i in range(0, len(m3)):
- for j in range(i+1, len(m3)):
-  if(m3[i] == m3[j]):
-   print("4.Mark with highest frequency:: ",m3[j])
+
+for i in m3.values():
+ for j in m3.values():
+  if(i==j):
+   print("4.Mark with highest frequency:: ",j)
    break
+  break
